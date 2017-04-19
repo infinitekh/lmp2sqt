fe61b85c9a40e669c545059007eaa7e557aeb2fb
Hyeok KIM <ekh0324@gmail.com>
2017-04-19 14:21:12 +0900

    Revert "Print out fqt functions like gnuplot matrix"
    This reverts commit ab970429e6829c508a2482165cd9d0be725dad84.

--- common.h
+++ common.h
@@ -16,6 +16,5 @@
  *
  * =====================================================================================
  */
-#include <stdio.h>
+
 typedef double real;
-int fwrite_matrix( FILE *fout, real **m, int xsize, int ysize, real *rt, real *ct);
